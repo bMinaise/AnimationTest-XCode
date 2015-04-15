bf88218a3a5b523893c6619d48c8ca541b048900
Hamza Siddiqui <hamzasiddiqui@Hamzas-MacBook-Pro.local>
2015-04-15 15:40:45 -0700

    Initial Commit

---
+++ AnimationTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationTest
+//
+//  Created by Hamza Siddiqui on 4/15/15.
+//  Copyright (c) 2015 Hamza Sidd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
